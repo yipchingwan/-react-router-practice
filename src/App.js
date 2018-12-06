18fa203f925f319226217e3e1ccecd18f904698a
KYLE\Surface <kyle.public10415@gmail.com>
2018-12-06 14:18:52 +0800

    feat : add aboutpage and notfound page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {Switch, Route, Link} from 'react-router-dom'
+import AboutPage from './components/AboutPage'
+import NotFoundPage from './components/NotFoundPage'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Router>
-          <div>
+        <nav>
             <ul>
               <li>
                 <Link to="/about">about</Link>
               </li>
             </ul>
-            <Route path="/about" component={()=> <h2>about</h2>}></Route>
-          </div>
-        </Router>
+        </nav>
+        <hr />
+        <Switch>
+          <Route path="/" exact component={()=><h1>Ikea's home page</h1>}></Route>
+          <Route path="/about" component={AboutPage}></Route>
+          <Route component={NotFoundPage}></Route>
+        </Switch>
+        
       </div>
     );
   }
