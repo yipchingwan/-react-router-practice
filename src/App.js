302bc603cc8e38b57e0999f3b36b23732ad320e3
KYLE\Surface <kyle.public10415@gmail.com>
2018-12-06 13:52:23 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/about">about</Link>
+              </li>
+            </ul>
+            <Route path="/about" component={()=> <h2>about</h2>}></Route>
+          </div>
+        </Router>
       </div>
     );
   }
